446e5d2680df8fb3837ceef7cedb7ea7df918ac8
si delv <151720755+creuserr@users.noreply.github.com>
2024-07-08 22:24:38 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,41 @@
+from http.server import BaseHTTPRequestHandler
+import requests, json, random, string
+
+def generate_version():
+  ver = []
+  for i in range(random.randint(1, 3)):
+    ver.append(str(random.randint(100, 999)))
+  return '.'.join(ver)
+
+def generate_model():
+  name = ''
+  for i in range(3):
+    name += random.choice(string.ascii_uppercase)
+  name += str(random.randint(100, 999))
+  return name
+
+def create_header():
+  moz = random.randint(3, 5)
+  android = random.randint(10, 14)
+  ver = random.randint(3, 5)
+  return { 'User-Agent': f'Mozilla/{moz}.0 (Linux; Android {android}; {generate_model()}) AppleWebKit/{generate_version()} (KHTML, like Gecko) Version/{ver}.0 Chrome/{generate_version()} Mobile Safari/{generate_version()}' }
+
+def fetch_proxy():
+  req = requests.get('https://proxylist.geonode.com/api/proxy-list?google=false&protocols=https&limit=5&page=1&sort_by=latency&sort_type=asc')
+  res = json.loads(req.text)['data']
+  return { 'https': [x['ip'] for x in res] }
+
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    result = None
+    try:
+      req = requests.get('https:/' + self.path, proxies=fetch_proxy(), headers=create_header())
+      self.send_response(200)
+      result = req.text
+    except Exception as e:
+      self.send_response(500)
+      result = str(e)
+    self.send_header('Content-Type', 'text/plain')
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.end_headers()
+    self.wfile.write(result.encode('utf-8'))
